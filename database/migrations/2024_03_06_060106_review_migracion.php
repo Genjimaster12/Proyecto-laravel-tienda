486bc2953a3588cd5399dbc1e1bdbe6e8a52e07a
Genjimaster12 <jjllg120702@gmail.com>
2024-03-06 00:23:46 -0600

    Creacion de la migracion con datos de tabla revies

--- database/migrations/2024_03_06_060106_review_migracion.php
+++ database/migrations/2024_03_06_060106_review_migracion.php
@@ -12,6 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         //
+        Schema::create('reviews', function(Blueprint $table){
+            $table->id('Id_reviews');
+            $table->string('Nombre_usuario');
+            $table->string('Nombre_Juego');
+            $table->integer('Puntuacion');
+            $table->string('Comentario');
+            $table->timestamps();
+        });
     }
 
     /**
